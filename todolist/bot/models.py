ad271c240749a6c3836c9a78954035c6605381c4
andry <mukhin2019@gmail.com>
2023-07-10 19:20:10 +0300

    add bot v2

--- todolist/bot/models.py
+++ todolist/bot/models.py
@@ -2,14 +2,12 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.crypto import get_random_string
 
-from core.models import User
-
 USER = get_user_model()
 
 
 class TgUser(models.Model):
     chat_id = models.BigIntegerField(primary_key=True, editable=False, unique=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(USER, on_delete=models.CASCADE, null=True, blank=True)
     verification_code = models.CharField(max_length=20, null=True, blank=True)
 
     def update_verification_cade(self) -> None:
