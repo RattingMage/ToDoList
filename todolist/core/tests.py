3f4b17ca285c2558a3490b771e25ebc2c59b1ebb
RattingMage <66022822+RattingMage@users.noreply.github.com>
2023-07-10 12:48:08 +0300

    Update tests.py v1

--- todolist/core/tests.py
+++ todolist/core/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from rest_framework.serializers import ValidationError
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .serializers import RegistrationSerializer
+
+User = get_user_model()
+
+
+class RegistrationSerializerTestCase(APITestCase):
+    def test_validate_passwords_match(self):
+        data = {
+            "password": "testpassword",
+            "password_repeat": "testpassword",
+        }
+        serializer = RegistrationSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        validated_data = serializer.validated_data
+        self.assertEqual(validated_data["password"], "testpassword")
+        self.assertEqual(validated_data["password_repeat"], "testpassword")
+
+    def test_validate_passwords_mismatch(self):
+        data = {
+            "password": "testpassword1",
+            "password_repeat": "testpassword2",
+        }
+        serializer = RegistrationSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn("non_field_errors", serializer.errors)
+        error_message = serializer.errors["non_field_errors"][0]
+        self.assertEqual(error_message, "Password and repeated password is not equal")
+
+    def test_create_user(self):
+        data = {
+            "username": "testuser",
+            "password": "testpassword",
+            "password_repeat": "testpassword",
+            # Дополнительные поля, необходимые для создания пользователя
+        }
+        serializer = RegistrationSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        user = serializer.save()
+        self.assertEqual(user.username, "testuser")
+        self.assertTrue(user.check_password("testpassword"))
+        # Дополнительные проверки полей пользователя, если необходимо
